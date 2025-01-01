5dd429b9689ded84c83b9ead22b60ef21763df0a
Sachin Zade <114475206+sachinz25@users.noreply.github.com>
2025-01-02 02:05:20 +0530

    Update BitmapProvider.kt

--- app/src/main/kotlin/app/vitune/android/service/BitmapProvider.kt
+++ app/src/main/kotlin/app/vitune/android/service/BitmapProvider.kt
@@ -1,103 +1,122 @@
-package it.fast4x.rimusic.service
+package app.vitune.android.service
 
-
+import android.content.Context
 import android.content.res.Configuration
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.core.graphics.applyCanvas
-import coil.imageLoader
-import coil.request.CachePolicy
-import coil.request.Disposable
-import coil.request.ImageRequest
-import it.fast4x.rimusic.utils.thumbnail
-import it.fast4x.rimusic.appContext
-import timber.log.Timber
+import app.vitune.android.utils.thumbnail
+import coil3.imageLoader
+import coil3.request.Disposable
+import coil3.request.ImageRequest
+import coil3.request.allowHardware
+import coil3.toBitmap
 
-//context(Context)
+context(Context)
 class BitmapProvider(
-    private val bitmapSize: Int,
-    private val colorProvider: (isSystemInDarkMode: Boolean) -> Int
+    private val getBitmapSize: () -> Int,
+    private val getColor: (isDark: Boolean) -> Int
 ) {
     var lastUri: Uri? = null
         private set
 
-    var lastBitmap: Bitmap? = null
+    private var lastBitmap: Bitmap? = null
+        set(value) {
+            field?.takeIf { !it.isRecycled }?.recycle()
+            field = value?.let { 
+                if (!it.isRecycled) {
+                    try {
+                        it.copy(it.config, true)
+                    } catch (e: Exception) {
+                        null
+                    }
+                } else null
+            }
+            listener?.invoke(field)
+        }
     private var lastIsSystemInDarkMode = false
-
-    private var lastEnqueued: Disposable? = null
+    private var currentTask: Disposable? = null
 
     private lateinit var defaultBitmap: Bitmap
+    val bitmap: Bitmap? 
+        get() = lastBitmap?.takeIf { !it.isRecycled } ?: defaultBitmap.takeIf { !it.isRecycled }
 
-    val bitmap: Bitmap
-        get() = lastBitmap ?: defaultBitmap
-
-    var listener: ((Bitmap?) -> Unit)? = null
-        set(value) {
-            field = value
-            value?.invoke(lastBitmap)
-        }
+    private var listener: ((Bitmap?) -> Unit)? = null
 
     init {
         setDefaultBitmap()
     }
 
     fun setDefaultBitmap(): Boolean {
-        val isSystemInDarkMode = appContext().resources.configuration.uiMode and
+        val isSystemInDarkMode = resources.configuration.uiMode and
                 Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
 
         if (::defaultBitmap.isInitialized && isSystemInDarkMode == lastIsSystemInDarkMode) return false
 
         lastIsSystemInDarkMode = isSystemInDarkMode
 
-        runCatching {
-            defaultBitmap =
-                Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888).applyCanvas {
-                    drawColor(colorProvider(isSystemInDarkMode))
-                }
-        }.onFailure {
-            Timber.e("Failed set default bitmap in BitmapProvider ${it.stackTraceToString()}")
+        val size = getBitmapSize()
+        defaultBitmap = Bitmap.createBitmap(
+            /* width = */ size,
+            /* height = */ size,
+            /* config = */ Bitmap.Config.ARGB_8888
+        ).applyCanvas {
+            drawColor(getColor(isSystemInDarkMode))
         }
 
         return lastBitmap == null
     }
 
-    fun load(uri: Uri?, onDone: (Bitmap) -> Unit) {
-        Timber.d("BitmapProvider load method being called")
+    fun load(
+        uri: Uri?,
+        onDone: (Bitmap) -> Unit = { }
+    ) {
         if (lastUri == uri) {
             listener?.invoke(lastBitmap)
             return
         }
 
-        lastEnqueued?.dispose()
+        currentTask?.dispose()
         lastUri = uri
 
-        runCatching {
-            lastEnqueued = appContext().imageLoader.enqueue(
-                ImageRequest.Builder(appContext())
-                    .networkCachePolicy(CachePolicy.ENABLED)
-                    .data(uri.thumbnail(bitmapSize))
-                    .allowHardware(false)
-                    .diskCacheKey(uri.thumbnail(bitmapSize).toString())
-                    //.memoryCacheKey(uri.thumbnail(bitmapSize).toString())
-                    .listener(
-                        onError = { _, result ->
-                            Timber.e("Failed to load bitmap ${result.throwable.stackTraceToString()}")
-                            lastBitmap = null
-                            onDone(bitmap)
-                            //listener?.invoke(lastBitmap)
-                        },
-                        onSuccess = { _, result ->
-                            lastBitmap = (result.drawable as BitmapDrawable).bitmap
-                            onDone(bitmap)
-                            //listener?.invoke(lastBitmap)
-                        }
-                    )
-
-                    .build()
-            )
-        }.onFailure {
-            Timber.e("Failed enqueue in BitmapProvider ${it.stackTraceToString()}")
+        if (uri == null) {
+            lastBitmap = null
+            onDone(bitmap)
+            return
         }
+
+        currentTask = applicationContext.imageLoader.enqueue(
+            ImageRequest.Builder(applicationContext)
+                .data(uri.thumbnail(getBitmapSize()))
+                .allowHardware(false)
+                .listener(
+                    onError = { _, _ ->
+                        lastBitmap = null
+                        onDone(bitmap)
+                    },
+                    onSuccess = { _, result ->
+                        lastBitmap = result.image.run { toBitmap(width, height) }
+                        onDone(bitmap)
+                    }
+                )
+                .build()
+        )
+    }
+
+    fun setListener(callback: ((Bitmap?) -> Unit)?) {
+        listener = callback
+        listener?.invoke(lastBitmap)
+    }
+
+    fun clear() {
+        currentTask?.dispose()
+        lastBitmap?.recycle()
+        lastBitmap = null
+        lastUri = null
+    }
+
+    override fun finalize() {
+        clear()
+        defaultBitmap.recycle()
     }
 }
